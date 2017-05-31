9c0de31abbc189529a754ccf12f1596eb0bd02c2
Andrew Jones <jones.gaffney1920@gmail.com>
2017-05-31 13:30:07 -0400

    First Commit

---
+++ src/accept/Division.java
@@ -0,0 +1,30 @@
+package accept;
+
+import java.util.Scanner;
+
+class Division {
+  public static void main(String[] args) {
+ 
+  int a, b, result;
+ 
+  Scanner input = new Scanner(System.in);
+  System.out.println("Input two integers");
+ 
+  a = input.nextInt();
+  b = input.nextInt();
+ try{
+	 
+	 	result = a / b;
+ 
+	 	System.out.println("Result = " + result);
+	 
+ 	
+  }catch (ArithmeticException e)
+	{
+		System.out.println("Error!");
+	}finally {
+		System.out.println("finally block will execute.");
+	}
+ input.close();
+  }
+}
